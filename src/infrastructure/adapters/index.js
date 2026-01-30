9116a26e2efd17c77f38f4ea10810e5414281abe
gurdevsinghvip@gmail.com <gurdevsinghvip@gmail.com>
2026-01-30 21:22:39 +0530

    refactor: correct import statements in index.js for consistency and clarity

--- src/infrastructure/adapters/index.js
+++ src/infrastructure/adapters/index.js
@@ -1,15 +1,17 @@
-import { KeyStoreAdapter } from "./keyStoreAdapter";
 import { MetadataStoreAdapter } from "./metadataStoreAdapter.js";
 import { GarbageStoreAdapter } from "./GarbageStoreAdapter.js";
 import { RotationPloicyAdapter } from "./rotationPolicyAdapter.js";
 import { ActiveKidStoreAdapter } from "./activeKidStoreAdapter.js";
 import { RotationLockAdapter } from "./rotationLockAdapter.js"
 import { CryptoEngineAdapter } from "./cryptoEngineAdapter.js"
-import { ActiveKidCache, rotationLockRepository } from "../cache/index.js";
-import { fileSystem } from "../filesystem/index.js";
+import { KeyStoreAdapter } from "./keyStoreAdapter.js";
+
+
 import { GarbageRepository, rotationPolicyRepository } from "../db/index.js";
+import { ActiveKidCache, rotationLockRepository } from "../cache/index.js";
 import { rotationLockRepository } from "../cache/index.js";
 import { cryptoEngine } from "../cryptoEngine/index.js";
+import { fileSystem } from "../filesystem/index.js";
 
 
 const keyStoreAdapter = new KeyStoreAdapter(fileSystem.keyStore);
@@ -24,7 +26,7 @@ const rotationLockAdapter = new RotationLockAdapter({ cache: rotationLockReposit
 
 const cryptoEngineAdapter = new CryptoEngineAdapter(cryptoEngine);
 
-export { 
+export {
     metadataStoreAdapter,
     keyStoreAdapter,
     garbageStoreAdapter,
@@ -32,4 +34,4 @@ export {
     rotationLockAdapter,
     activeKidStoreAdapter,
     cryptoEngineAdapter
- };
+};
